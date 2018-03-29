a36ac59f48a62d88f1e310b2fd3133b2cf4e7eee
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-03-29 21:18:09 +0700

    chore(package): update xo to version 0.20.3 (#5)
    * chore(package): update xo to version 0.20.3
    
    Closes #3
    
    * refactor: adapt changes to the latest `xo`

--- test.js
+++ test.js
@@ -3,27 +3,25 @@
 const { homedir } = require('os');
 const { join } = require('path');
 
-const gitDirUp = require('./');
+const gitDirUp = require('.');
 
-describe('find `.git` dir', () => {
-  it('can find `.git` dir from `fixtures`', async () => {
-    expect.assertions(1);
-    const gitDir = await gitDirUp('fixtures');
+it('can find `.git` dir from `fixtures`', async () => {
+  expect.assertions(1);
+  const gitDir = await gitDirUp('fixtures');
 
-    expect(gitDir).toBe(join(__dirname, '.git'));
-  });
+  expect(gitDir).toBe(join(__dirname, '.git'));
+});
 
-  it('can find `.git` dir from `process.cwd()`', async () => {
-    expect.assertions(1);
-    const gitDir = await gitDirUp();
+it('can find `.git` dir from `process.cwd()`', async () => {
+  expect.assertions(1);
+  const gitDir = await gitDirUp();
 
-    expect(gitDir).toBe(join(__dirname, '.git'));
-  });
+  expect(gitDir).toBe(join(__dirname, '.git'));
+});
 
-  it("can't find `.git` dir from `homedir()`", async () => {
-    expect.assertions(1);
-    const gitDir = await gitDirUp(homedir());
+it("can't find `.git` dir from `homedir()`", async () => {
+  expect.assertions(1);
+  const gitDir = await gitDirUp(homedir());
 
-    expect(gitDir).toBe(null);
-  });
+  expect(gitDir).toBe(null);
 });
